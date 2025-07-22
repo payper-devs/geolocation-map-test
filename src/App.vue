f55ced3dea774bf7e31eb364549de85ccec5e12f
Gyehyun Park <gyehyunbak@gmail.com>
2025-07-22 16:08:08 +0900

    feature: init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useGeolocation } from "@vueuse/core";
+
+const { coords } = useGeolocation();
+</script>
+
+<template>
+  <div>
+    <p>coords: {{ coords }}</p>
+  </div>
+</template>
